81044182cac47e863e73deee843f44eb83d1d17a
Neet <75447256+Chhez@users.noreply.github.com>
2024-07-12 16:04:30 -0700

    Imported bootstrap

--- Website/Frontend/src/components/Nbar.jsx
+++ Website/Frontend/src/components/Nbar.jsx
@@ -1,11 +1,10 @@
 import styles from './navbar.module.scss';
 import { Link, Outlet } from 'react-router-dom';
+import { Navbar, Nav } from 'react-bootstrap';
 
 function Nbar() {
     return (<>
-        <h1>Navbar</h1>
-        <Link to="/shop">Click me!</Link>
-
+        
         <Outlet />
     </>);
 }
