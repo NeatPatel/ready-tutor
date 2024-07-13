ef7a82d9dce196547538232e525a8656fc9bc4dc
Neet <75447256+Chhez@users.noreply.github.com>
2024-07-12 23:57:42 -0700

    Landing Page update

--- Website/Frontend/src/pages/Home.jsx
+++ Website/Frontend/src/pages/Home.jsx
@@ -1,16 +1,22 @@
 import styles from './home.module.scss';
 import ContentBlock from '../components/ContentBlock.jsx';
-import { Button } from 'react-bootstrap';
-import img from '../assets/logo.png'
+import { Button, Image } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import alex_cover from '../assets/alex.png';
 
 function Home() {
     return (<>
         <ContentBlock mt="25" mb="0" content={<h1 className="display-5 text-light">An educational <span className="text-info">experience</span><br /> like no other</h1>} />
-        <ContentBlock mt="0" mb="25" content={<p className="text-light">
+        <ContentBlock mt="0" mb="0" content={<p className="text-light">
             Ready Tutor is a virtual educational service for K-12 and college students looking to <br />further their 
             educational experience right here, right now.</p>} />
-        <ContentBlock mt="25" mb="25" content={<Button variant="outline-info">An educational experience like none other</Button>} />
-        <ContentBlock mt="25" mb="25" content={<img src={img} alt="An educational experience like none other" width="200vh" height="auto" />} />
+        <ContentBlock mt="5" mb="25" content={<Link to="/pricing"><Button variant="outline-info" size="lg">Get Started</Button></Link>} />
+        <ContentBlock mt="20" mb="25" content={<Image src={alex_cover} alt="An educational experience like none other" fluid rounded />} />
+        <ContentBlock mt="25" mb="25" content={<>
+            <h2 className="display-5 text-light">⁕</h2>
+            <h1 className="display-4 text-light">A <span className="text-info">virtual army</span> of tutors</h1>
+            <p className="text-light">Our professional tutoring services range across a wide variety of subjects, <br />from electrodynamical physics to 18th century world history.</p>
+        </>} />
     </>);
 }
 
