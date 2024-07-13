022e157782acaf124408090281e3538d0416fbda
FishReddy <vishwareddym@gmail.com>
2024-07-12 21:52:59 -0700

    updated about spacing

--- Website/Frontend/src/pages/About.jsx
+++ Website/Frontend/src/pages/About.jsx
@@ -6,13 +6,13 @@ import waggImg from '../assets/wagg.jpg';
 function About() {
     return (<>
         <h1 className= 'text-dark text-center mt-2'>About Us</h1>
-        <SplitContentBlock mt='25' mb='25' left={<h2 className= 'text-center m-auto'>About</h2>} right={<p className= 'text-center m-auto'>Some of the hardest things that people have to go through can be things such as academia itself. The world of learning is a tough place, and navigating it can sometimes lead to a lot of stress and anxiety.
+        <SplitContentBlock mt='25' mb='30' left={<h2 className= 'text-center m-auto'>About</h2>} right={<p className= 'text-center m-auto'>Some of the hardest things that people have to go through can be things such as academia itself. The world of learning is a tough place, and navigating it can sometimes lead to a lot of stress and anxiety.
 
 Why navigate alone? Here at Ready Tutor, we are here to help you out on your academic journey and all problems that you facing, with examples and stories to help you learn and remember everything that you need!
 
 </p>} />
-        <SplitContentBlock mt='25' mb='25' left={<p className= 'text-center m-auto'>From a High School Physics Club to a K-12 Virtual E-business, our business started as simple private tutoring, where the needs of students that could not understand course content were aided through our efforts.</p>} right={<h1 className= 'text-center m-auto'>Our Story</h1>} />
-        <SplitContentBlock mt='25' mb='25' left={<h2 className= 'text-center m-auto'>Why Wait? Get a Free Consultation Today!</h2>} right={<Image src={waggImg} fluid />}/>
+        <SplitContentBlock mt='35' mb='30' left={<p className= 'text-center m-auto'>From a High School Physics Club to a K-12 Virtual E-business, our business started as simple private tutoring, where the needs of students that could not understand course content were aided through our efforts.</p>} right={<h1 className= 'text-center m-auto'>Our Story</h1>} />
+        <SplitContentBlock mt='30' mb='25' left={<h2 className= 'text-center m-auto'>Why Wait? Get a Free Consultation Today!</h2>} right={<Image src={waggImg} fluid />}/>
         </>);
 }
 
