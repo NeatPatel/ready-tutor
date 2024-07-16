34c8922364edea8022e109780b12024fea074a60
Neet <75447256+Chhez@users.noreply.github.com>
2024-07-16 15:09:09 -0700

    Added Google Form Contact

--- Website/Frontend/src/pages/ContactUs.jsx
+++ Website/Frontend/src/pages/ContactUs.jsx
@@ -10,11 +10,14 @@ function ContactUs() {
     const { pathName } = useLocation();
 
     return (<>
-        <ContentBlock mt='25' mb='0' content={<><h1 className= 'm-auto display-1 text-light'>Contact Us</h1><br/><p className='text-center text-light'>Thank you for your interest in Ready Tutor! You can set up a quick meeting or a one-on-one free consultation today with quick and easy calendar process!</p></>}/>
+        <ContentBlock mt='25' mb='0' content={<><h1 className= 'm-auto display-1 text-light'>Contact Us</h1><br/><p className='text-center text-light'>Thank you for your interest in Ready Tutor! We have <span className="text-info">multiple options</span> for you to choose from for contact:</p></>}/>
 
         <ContentBlock mt="0" mb="0" content={<>
             <p className="text-light">Calendly not showing up? <a href="https://calendly.com/neetp287">Try this link instead</a></p>
             <InlineWidget url="https://calendly.com/neetp287" />
+            <hr style={{border: "3px solid #ffffff"}} />
+            <p className="text-center text-light">OR try this:</p>
+            <iframe src="https://docs.google.com/forms/d/e/1FAIpQLScbnkakgMsgrxi6WXvvhyRI09yvtmidQjLvRjq-X2QzbzqxvQ/viewform?embedded=true" width="640" height="3735" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
         </>} />
     </>);
 }
