f7b9a24a2a436bed3fe2aa10dd49bae3d8e4d0fd
Neet <75447256+Chhez@users.noreply.github.com>
2024-12-10 16:19:07 -0800

    Added click away feature

--- Website/Frontend/src/components/Nbar.jsx
+++ Website/Frontend/src/components/Nbar.jsx
@@ -12,24 +12,26 @@ function Nbar() {
         setToggle(isToggled);
     };
 
-    const setToggler = () => {
-        setToggle(false);
-    };
+    window.addEventListener("click", (e) => {
+        if(e.target.tagName != "SPAN" && e.target.id != "navBarIdTogglePiece") {
+            setToggle(false);
+        }
+    });
 
     return (<>
         <Navbar id="mainNavBar" onToggle={toggled} expanded={toggle} variant="light" fixed="top" className="mx-4 navbar-light bg-light border-primary" expand="lg">
             <Container fluid>
                 <Navbar.Brand>
-                    <Link to="/" onClick={setToggler} className="nav-link"><img src={logo} height="50vh" className="d-inline-block align-self-center" /></Link>
+                    <Link to="/" className="nav-link"><img src={logo} height="50vh" className="d-inline-block align-self-center" /></Link>
                 </Navbar.Brand>
-                <Navbar.Toggle aria-controls="mainNavPanel" />
+                <Navbar.Toggle id="navBarIdTogglePiece" aria-controls="mainNavPanel" />
                 <Navbar.Collapse className="text-end" id="mainNavPanel">
                     <Nav className="ms-auto d-inline" navbarScroll>
-                        <Link onClick={setToggler} to="/about" className="nav-link my-auto d-inline-block">About</Link>
+                        <Link to="/about" className="nav-link my-auto d-inline-block">About</Link>
                         <span className="mx-1" />
-                        <Link onClick={setToggler} to="/pricing" className="nav-link my-auto d-inline-block">Pricing</Link>
+                        <Link to="/pricing" className="nav-link my-auto d-inline-block">Pricing</Link>
                         <span className="mx-1" />
-                        <Link onClick={setToggler} to="/contact-us" className="nav-link d-inline-block"><Button variant="outline-danger">Book Appointment</Button></Link>
+                        <Link to="/contact-us" className="nav-link d-inline-block"><Button variant="outline-danger">Book Appointment</Button></Link>
                     </Nav>
                 </Navbar.Collapse>
             </Container>
